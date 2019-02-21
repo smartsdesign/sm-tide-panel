29eb778f748ed9419769f63b354c3c123ede4507
Samson Muhangi <smuhangi@discovery.com>
2019-02-21 00:00:03 +0000

    updates styles

--- src/app/data/accounts.ts
+++ src/app/data/accounts.ts
@@ -24,6 +24,18 @@ const custAccounts: IAccount[] = [
         lastName: 'Stefanov',
         dob: new Date(1999, 11, 25),
         action: 'edit'
+    },
+    {
+        firstName: 'Wes',
+        lastName: 'Bos',
+        dob: new Date(1989, 11, 26),
+        action: 'edit'
+    },
+    {
+        firstName: 'Rachel',
+        lastName: 'Navatolova',
+        dob: new Date(1989, 7, 26),
+        action: 'edit'
     }
 ];
 
