15a8720177d05b7b1195ae88dfcbaecfe8ba88bd
Samson Muhangi <samson.muhangi@tide.co>
2019-04-26 15:26:57 +0100

    adds version element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, isDevMode, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 @Component({
     selector: 'tide-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-    title = 'TIDE accounts';
+export class AppComponent implements OnInit {
+    public title: string = 'TIDE accounts';
+    public appVersion: string = environment.version;
+    public isProd: boolean;
+
+    ngOnInit(): void {
+        this.isProd = isDevMode ? false : true;
+        console.log(this.isProd);
+    }
 }
